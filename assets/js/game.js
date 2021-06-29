34f241e340736b2ecf4fed74b367b2358bc0d9ee
zacktmiller92 <zacktmiller92@gmail.com>
2021-06-28 20:14:42 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -61,7 +61,7 @@ var fight = function(enemyName) {
       // leave while() loop if player is dead
       break;
     } else {
-      window.alert(playerName + ' still has ' + playerHealth + ' health left.');
+       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
   }
 };
@@ -70,9 +70,15 @@ var fight = function(enemyName) {
 
 
 for(var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+  } else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+  }
 }
 
 
