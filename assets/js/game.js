46b1a30618dac6297a4ef9455377a85fbf0f32cb
zacktmiller92 <zacktmiller92@gmail.com>
2021-06-29 08:48:27 -0400

    created startGame and endGame functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,21 +68,47 @@ var fight = function(enemyName) {
 
 
 
-
-for(var i = 0; i < enemyNames.length; i++) {
-  if (playerHealth > 0) {
-    window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-  } else {
-      window.alert("You have lost your robot in battle! Game Over!");
-      break;
+// function to start a new game
+var startGame = function() {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+      var pickedEnemyName = enemyNames[i];
+      enemyHealth = 50;
+      fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
   }
-}
+  endGame();
+};
+
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    // restart the game
+    startGame();
+  } 
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
 
 
-// https://courses.bootcampspot.com/courses/652/pages/3-dot-2-7-battle-the-combatants-until-defeated-using-a-while-loop?module_item_id=223392
-// 3.2.7
-// break out of the loop HTMLTableSection
+
+startGame();
 
